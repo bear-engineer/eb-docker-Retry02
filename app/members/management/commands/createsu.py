692ded078d4daa1cda25d9f18c8d4f14dd5c3fd4
Sehyeon Youn <d.sehyeon@gmail.com>
2018-07-17 20:40:45 +0900

    superuser backends settings

---
+++ app/members/management/commands/createsu.py
@@ -0,0 +1,16 @@
+import json
+import os
+
+from django.conf import settings
+from django.contrib.auth import get_user_model
+from django.core.management import BaseCommand
+User = get_user_model()
+
+class Command(BaseCommand):
+    secrets = json.load(open(os.path.join(settings.SECRETS_DIR, 'data.json')))
+    if not User.objects.filter(username=secrets['SUPERUSER_NAME']).exists():
+        User.objects.create_superuser(
+            username=secrets['SUPERUSER_NAME'],
+            password=secrets['SUPERUSER_PASSWORD'],
+            email=secrets['SUPERUSER_EMAIL'],
+        )
--- app/members/models.py
+++ app/members/models.py
@@ -1,8 +1,12 @@
-from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.contrib.auth.models import AbstractUser, UserManager as DjangoUserManager
+
+class UserManager(DjangoUserManager):
+    pass
 
 class User(AbstractUser):
     profile_img = models.ImageField(upload_to='profile')
+    objects = UserManager()
 
     def __str__(self):
         return self.username
